0b827eedbabc01635061e112bbff8ac9966b90cc
Samuel Redd <samredd@samuels-air-103.lan>
2024-09-24 23:18:43 -0600

    game test start

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -96,7 +96,7 @@ public class ChessGame {
      * @param board the new board to use
      */
     public void setBoard(ChessBoard board) {
-        throw new RuntimeException("Not implemented");
+        board.resetBoard();
     }
 
     /**
