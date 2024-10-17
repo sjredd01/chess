557f3959b6ff14ce5cd071c8d97f3e728190b9eb
Samuel Redd <samredd@samuels-air-103.lan>
2024-10-16 22:59:30 -0600

    Game dataaccess stuff

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -1,5 +1,15 @@
 package dataaccess;
 
+import model.GameData;
+
+import java.util.HashSet;
+
 public interface GameDAO {
 
+    HashSet<GameData> listGames();
+    void createGame(GameData game);
+    GameData getGame(int gameId) throws DataAccessException;
+    boolean gameExists(int gameId);
+    void updateGame(GameData game) throws DataAccessException;
+    void clear();
 }
--- server/src/main/java/dataaccess/MemoryGameDAO.java
+++ server/src/main/java/dataaccess/MemoryGameDAO.java
@@ -1,5 +1,62 @@
 package dataaccess;
 
+import model.GameData;
+
+import java.util.HashSet;
+
 public class MemoryGameDAO implements GameDAO{
 
+    private HashSet<GameData> db;
+
+    public MemoryGameDAO(){
+        db = HashSet.newHashSet(10);
+    }
+
+    @Override
+    public HashSet<GameData> listGames() {
+        return db;
+    }
+
+    @Override
+    public void createGame(GameData game){
+        db.add(game);
+    }
+
+    @Override
+    public GameData getGame(int gameId) throws DataAccessException {
+        for(GameData game : db){
+            if(game.gameID() == gameId){
+                return game;
+            }
+        }
+
+        throw new DataAccessException("Game does not exist");
+    }
+
+    @Override
+    public boolean gameExists(int gameId) {
+
+        for(GameData game : db){
+            if(game.gameID() == gameId){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void updateGame(GameData game) throws DataAccessException {
+        try{
+            db.remove(getGame(game.gameID()));
+            db.add(game);
+        } catch (DataAccessException e) {
+            db.add(game);
+        }
+
+    }
+
+    @Override
+    public void clear() {
+        db = HashSet.newHashSet(10);
+    }
 }
