796aa65f9c173fc77ebef3972e4b920eac76e1c0
Samuel Redd <samredd@Samuels-Air-103.app.byu.edu>
2024-11-04 14:57:03 -0700

    repl works

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -2,10 +2,35 @@ package ui;
 
 import server.ServerFacade;
 
+import java.util.Arrays;
+
 public class ChessClient {
     private final ServerFacade server;
 
     public ChessClient(String serverURL){
         server = new ServerFacade(serverURL);
+    }
+
+
+    public String help(){
+        return """
+                register <USERNAME> <PASSWORD> <EMAIL>
+                login <USERNAME> <PASSWORD>
+                quit
+                help
+                """;
+    }
+
+    public String eval(String line) {
+        try{
+            var token = line.toLowerCase().split(" ");
+            var cmd = (token.length > 0) ? token[0] : "help";
+            var param = Arrays.copyOfRange(token, 1, token.length);
+            return switch (cmd){
+                default -> help();
+            };
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -1,9 +1,32 @@
 package ui;
 
+import java.util.Scanner;
+
 public class Repl {
     private final ChessClient client;
 
     public Repl(String serverURL) {
         this.client = new ChessClient(serverURL);
+    }
+
+    public void run(){
+        System.out.println(client.help());
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            //printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+
+               result = client.eval(line);
+               System.out.print(result);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        System.out.println();
     }
 }
