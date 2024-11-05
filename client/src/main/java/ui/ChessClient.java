3ad2985ad6cc0402868262a08ff1f732067832f8
Samuel Redd <samredd@samuels-air-103.lan>
2024-11-04 18:34:47 -0700

    got register user to work

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -1,15 +1,20 @@
 package ui;
 
+import exception.ResponseException;
+import model.UserData;
 import server.ServerFacade;
 
 import java.util.Arrays;
 
 public class ChessClient {
     private final ServerFacade server;
+    private final String serverURL;
     private State state = State.LOGGEDOUT;
+    private ServerFacade sf;
 
     public ChessClient(String serverURL){
         server = new ServerFacade(serverURL);
+        this.serverURL = serverURL;
     }
 
 
@@ -39,10 +44,36 @@ public class ChessClient {
             var cmd = (token.length > 0) ? token[0] : "help";
             var param = Arrays.copyOfRange(token, 1, token.length);
             return switch (cmd){
+                case "register" -> registerUser(param);
+                case "login" -> logIn(param);
                 default -> help();
             };
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private String registerUser(String... param) throws ResponseException {
+        if(param.length >= 3){
+            var username = param[0];
+            var password = param[1];
+            var email = param[2];
+            var newUser = new UserData(username, password, email);
+            newUser = server.registerUser(newUser);
+
+            return newUser + " is now registered";
+        }
+
+        throw new ResponseException(400, "Expected <USERNAME> <PASSWORD> <EMAIL>");
+    }
+
+    private String logIn(String[] param) throws ResponseException {
+        if(param.length >= 1){
+            state = State.LOGGEDIN;
+            sf = new ServerFacade(serverURL);
+            return "you are signed in ";
+        }
+
+        throw new ResponseException(400, "Expected other");
     }
 }
--- shared/src/main/java/server/ServerFacade.java
+++ shared/src/main/java/server/ServerFacade.java
@@ -1,10 +1,66 @@
 package server;
 
+import com.google.gson.Gson;
+import model.UserData;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+
 public class ServerFacade {
 
     private final String serverURL;
 
     public ServerFacade(String serverURL) {
         this.serverURL = serverURL;
+    }
+
+    public UserData registerUser(UserData newUser) {
+        var path = "/user";
+        return this.makeRequest("POST", path, newUser, UserData.class);
+    }
+
+    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) {
+        try{
+            URL url = (new URI(serverURL + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
+
+            writeBody(request, http);
+            http.connect();
+            return readBody(http, responseClass);
+        } catch (URISyntaxException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
+        T response = null;
+        if(http.getContentLength() < 0){
+            try (InputStream resBody = http.getInputStream()){
+                InputStreamReader reader = new InputStreamReader(resBody);
+                if(responseClass != null){
+                    response = new Gson().fromJson(reader, responseClass);
+                }
+
+            }
+        }
+
+        return response;
+    }
+
+    private void writeBody(Object request, HttpURLConnection http) throws IOException {
+        if(request != null){
+            http.addRequestProperty("Content-Type", "application/json");
+            String data = new Gson().toJson(request);
+            try(OutputStream body = http.getOutputStream()){
+                body.write(data.getBytes());
+            }
+
+        }
     }
 }
